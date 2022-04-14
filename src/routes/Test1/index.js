a56c8450de49795327fcfa5af94be46c80b6f609
arjuntokopedia <ari.juanda@tokopedia.com>
2022-04-14 14:37:22 +0700

    feat: solution test 1

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+
 const Test1 = () => {
-  return(
+  const [input, setInput] = useState('');
+  return (
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> {input}
       </p>
-      <input type="text" placeholder="input here"/>
+      <input
+        type="text"
+        placeholder="input here"
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Test1;
